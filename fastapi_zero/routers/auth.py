93061c6a5a0837e17d9c079e1cc06a62b688bca0
Gabriel Reis <gabrielreispr@unochapeco.edu.br>
2025-12-02 16:23:58 -0300

    Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.

---
+++ fastapi_zero/routers/auth.py
@@ -0,0 +1,43 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User
+from fastapi_zero.security import (
+    create_access_token,
+    verify_password,
+)
+
+OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+Session = Annotated[Session, Depends(get_session)]
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+
+@router.post('/token')
+def login_for_acess_token(
+    form_data: OAuth2Form,
+    session: Session,
+):
+    user = session.scalar(
+        select(User).where((User.email == form_data.username))
+    )
+
+    exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Incorrect email or password',
+    )
+
+    if not user:
+        raise exception
+
+    if not verify_password(form_data.password, user.password):
+        raise exception
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'Bearer'}
