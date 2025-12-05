b6b435b5a6725b16ed06cc603098e9489bd300ea
Gabriel Reis <gabrielreispr@unochapeco.edu.br>
2025-12-05 18:19:07 -0300

    pequena atualização async

--- fastapi_zero/database.py
+++ fastapi_zero/database.py
@@ -1,11 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from aqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 from fastapi_zero.settings import Settings
 
-engine = create_engine(Settings().DATABASE_URL)
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
-def get_session():
-    with Session(engine) as session:
+async def get_session():
+    with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
