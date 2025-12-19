c4289576b2ad7b95aed5f78370dd1c5525dc4f1b
Gabriel Reis <gabrielreispr@unochapeco.edu.br>
2025-12-19 17:24:20 -0300

    refatorando alembic para async

--- fastapi_zero/routers/auth.py
+++ fastapi_zero/routers/auth.py
@@ -8,14 +8,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User
+from fastapi_zero.schemas import Token
 from fastapi_zero.security import (
     create_access_token,
+    get_current_user,
     verify_password,
 )
 
 OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
 Session = Annotated[AsyncSession, Depends(get_session)]
 router = APIRouter(prefix='/auth', tags=['auth'])
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/token')
@@ -41,3 +44,10 @@ async def login_for_acess_token(
     access_token = create_access_token(data={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'Bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+async def refresh_access_token(user: CurrentUser):
+    new_access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': new_access_token, 'token_type': 'bearer'}
